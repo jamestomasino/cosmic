d1f8110e2d9341cf35aa0b0ddfdd7eed37fd664f
James Tomasino <james@tomasino.org>
2018-11-20 22:50:36 -0500

    stubbed out log

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ if [ ! -L "/usr/local/bin/cosmicuser" ]; then
   ln -s "${DIR}/cosmicuser" "/usr/local/bin/cosmicuser"
 fi
 
+if [ ! -L "/usr/local/bin/log" ]; then
+  ln -s "${DIR}/log" "/usr/local/bin/log"
+fi
+
 if [ ! -L "/etc/welcomemail.tmpl" ]; then
   ln -s "${DIR}/welcomemail.tmpl" "/etc/welcomemail.tmpl"
 fi
