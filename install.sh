61cb492111587283631e109ff0dead6e4a8ed56d
James Tomasino <james@tomasino.org>
2018-11-24 18:27:55 -0500

    restructured repo

--- install.sh
+++ install.sh
@@ -6,40 +6,40 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 DIR=$(cd "$DIR" && pwd -P)
 
 if [ ! -L "/usr/local/bin/cosmic-user" ]; then
-  ln -s "${DIR}/cosmic-user" "/usr/local/bin/cosmic-user"
+  ln -s "${DIR}/bin/cosmic-user" "/usr/local/bin/cosmic-user"
 fi
 
 if [ ! -L "/usr/local/bin/cosmic-rss" ]; then
-  ln -s "${DIR}/cosmic-rss" "/usr/local/bin/cosmic-rss"
+  ln -s "${DIR}/bin/cosmic-rss" "/usr/local/bin/cosmic-rss"
 fi
 
 if [ ! -L "/usr/local/bin/cosmic-web" ]; then
-  ln -s "${DIR}/cosmic-web" "/usr/local/bin/cosmic-web"
+  ln -s "${DIR}/bin/cosmic-web" "/usr/local/bin/cosmic-web"
 fi
 
 if [ ! -L "/usr/local/bin/cosmic-ship" ]; then
-  ln -s "${DIR}/cosmic-ship" "/usr/local/bin/cosmic-ship"
+  ln -s "${DIR}/bin/cosmic-ship" "/usr/local/bin/cosmic-ship"
 fi
 
 if [ ! -L "/usr/local/bin/cosmic-roster" ]; then
-  ln -s "${DIR}/cosmic-roster" "/usr/local/bin/cosmic-roster"
+  ln -s "${DIR}/bin/cosmic-roster" "/usr/local/bin/cosmic-roster"
 fi
 if [ ! -L "/usr/local/bin/motd" ]; then
-  ln -s "${DIR}/cosmic-motd" "/usr/local/bin/motd"
+  ln -s "${DIR}/bin/cosmic-motd" "/usr/local/bin/motd"
 fi
 
 if [ ! -L "/usr/local/bin/log" ]; then
-  ln -s "${DIR}/log" "/usr/local/bin/log"
+  ln -s "${DIR}/bin/cosmic-log" "/usr/local/bin/log"
 fi
 
 if [ ! -L "/etc/welcomemail.tmpl" ]; then
-  ln -s "${DIR}/welcomemail.tmpl" "/etc/welcomemail.tmpl"
+  ln -s "${DIR}/templates/welcomemail.tmpl" "/etc/welcomemail.tmpl"
 fi
 
 if [ ! -L "/etc/newship.tmpl" ]; then
-  ln -s "${DIR}/newship.tmpl" "/etc/newship.tmpl"
+  ln -s "${DIR}/templates/newship.tmpl" "/etc/newship.tmpl"
 fi
 
 if [ ! -L "/etc/motd" ]; then
-  ln -s "${DIR}/motd" "/etc/motd"
+  ln -s "${DIR}/files/motd" "/etc/motd"
 fi
