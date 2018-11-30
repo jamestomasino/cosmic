d91ac9f95cdd5d0dfb2e62534cf5af54ff916de9
James Tomasino <james@tomasino.org>
2018-11-30 14:57:57 -0500

    prepping log to get much, much cooler

---
+++ completion/cosmic-log.d
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+_log() {
+  local cur
+  cur=${COMP_WORDS[COMP_CWORD]}
+  local helplist
+  helplist=$(log -z)
+  COMPREPLY=( $( compgen -W "$helplist" -- "$cur" ) )
+}
+
+# Detect if current shell is ZSH, and if so, load this file in bash
+# compatibility mode.
+if [ -n "$ZSH_VERSION" ]; then
+  autoload bashcompinit
+  bashcompinit
+fi
+
+complete -o default -o nospace -F _log log
